e50cf088cfe36b62b27cd08c11fcf50df102f243
Anastasia Staroverova <staroverova111211@mail.ru>
2025-05-27 18:18:29 +0300

    Mesto. Создание и удаление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
+function makeCard(title, imgUrl, cardDelete) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+
+    const cardTitle = card.querySelector('.card__title');
+    const cardImg = card.querySelector('.card__image');
+    const cardBtn = card.querySelector('.card__delete-button')
+
+    cardTitle.textContent = title
+    cardImg.src = imgUrl
+    cardBtn.addEventListener('click', cardDelete)
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function cardDelete(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+    initialCards.forEach((cardData) => {
+        const cardItem = makeCard(cardData.name, cardData.link, cardDelete)
+        cardsList.append(cardItem)
+    })
+}
+
+renderCards();
